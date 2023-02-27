bbe5b120ff6302ad362e53d6f12e85663a3055df
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-02-27 16:07:27 +0100

    creamos la funcion productor

--- consumidores_productores.py
+++ consumidores_productores.py
@@ -6,3 +6,14 @@ sem = threading.Semaphore(0)
 mutex = threading.Semaphore(1)
 buffer = []
 
+def productor():
+    global buffer
+    while True:
+        time.sleep(random.random())
+        dato = random.randint(1, 100)
+        mutex.acquire()
+        buffer.append(dato)
+        print ("productor:", dato)
+        mutex.release()
+        sem.release()
+
