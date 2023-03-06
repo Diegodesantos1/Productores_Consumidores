172510c9d0df871100e063898a01625aa5fa84a7
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-03-06 16:12:48 +0100

    función cliente y creada la cola

--- productores_consumidores.py
+++ productores_consumidores.py
@@ -2,3 +2,14 @@ from queue import Queue # Importamos la clase Queue
 from threading import Thread # Importamos la clase Thread
 import time # Importamos la librería time
 
+q = Queue(10) # Creamos una cola de 10 elementos
+
+def productor(nombre): # Definimos la función productor
+    numero = 1 # Creamos una variable que nos servirá para contar las pizzas
+    while True: # Creamos un bucle infinito
+        q.join() # Esperamos a que la cola esté vacía
+        q.put(numero) # Añadimos un elemento a la cola
+        print(f"{nombre} está produciendo la pizza {numero}") # Mostramos un mensaje
+        numero += 1 # Incrementamos el contador
+
+
